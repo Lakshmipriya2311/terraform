0e21d2b3796fd3e295f8fbad0d9f09ad9264f98d
Lakshmipriya2311 <118303293+Lakshmipriya2311@users.noreply.github.com>
2022-11-16 10:50:26 +0100

    first commit

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "subnet_id_1" {
+
+  type = string
+
+  default = "subnet-02a44e209d4ce3063"
+
+ }
+ variable "subnet_id_2" {
+
+  type = string
+
+  default = "subnet-0bb71b14040988511"
+
+ }
+ variable "subnet_id_3" {
+  
+  type = string
+
+  default = "subnet-02e1dbe2035a7927f"
+
+ }
+
+ variable "vpc_id" {
+  type = string
+  default = "vpc-009927863ddee3742"
+   
+ }
